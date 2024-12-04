8f28ffce5e43b23bbef8209ccb294b8bf75cde36
Rudra-iit <bsse1518@iit.du.ac.bd>
2024-12-04 18:59:27 +0600

    Create ludo.c

---
+++ ludo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct a {
+    int red, blue, green, yellow;
+} a;
+
+void normal ();
+void snake ();
+void labyrinth ();
+
+int main () {
+    return 0;
+}
