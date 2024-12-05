3f9b7a145442eae943b852cce6cb1244401b1467
Rudra-iit <bsse1518@iit.du.ac.bd>
2024-12-05 11:08:21 +0600

    Graphics library dowloaded

--- ludo.c
+++ ludo.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <graphics.h>
 
 struct a {
     int red, blue, green, yellow;
 } a;
+int snake[2];
+int ladder[2];
+int board[21][21];
+int dice;
 
-void normal ();
+void normal () {
+    a.red = board[20][9];
+    if (dice!=6) {
+        if (dice==1) {
+            a.red = board[20][dice];
+        }
+    }
+    a.blue = board[9][0];
+    a.yellow = board[0][9];
+    a.green = board[9][20];
+}
 void snake ();
 void labyrinth ();
 
