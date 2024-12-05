5264439859ef4e33500de5c18f17206de23b41b0
Rudra-iit <bsse1518@iit.du.ac.bd>
2024-12-05 12:04:37 +0600

    C++, not C

---
+++ ludo.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <graphics.h>
+
+struct a {
+    int red, blue, green, yellow;
+} a;
+int snake[2];
+int ladder[2];
+int board[21][21];
+int dice;
+
+void normal () {
+    a.red = board[20][9];
+    if (dice!=6) {
+        if (dice==1) {
+            a.red = board[20][dice];
+        }
+    }
+    a.blue = board[9][0];
+    a.yellow = board[0][9];
+    a.green = board[9][20];
+}
+void snakeMode ();
+void labyrinth ();
+
+int main () {
+    return 0;
+}
