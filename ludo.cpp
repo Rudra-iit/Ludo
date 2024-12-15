affcc1f6eaeafbec31205df178dde031e9aa10aa
Rudra-iit <bsse1518@iit.du.ac.bd>
2024-12-15 19:06:16 +0600

    First successful matrix accessing.
    And even changing one index's value from user input.

--- ludo.cpp
+++ ludo.cpp
@@ -1,25 +1,43 @@
 #include <stdio.h>
 
 char matrix[20][20];
+int i=0, j=0;
 
 void maze_1 () {
     FILE *f = fopen ("maze_1.txt", "r");
-    int i, j;
-    char dice;
+    char c;
     if (f==NULL) {
         printf ("Maze not found!\n");
+        return;
     }
-    for (i=0; i<20; i++) {
-        for (j=0; j<20; j++) {
-            while ((matrix[i][j] = fgetc (f)) != EOF) {   
-                printf ("%c", matrix[i][j]);
-            }
+    while ((c = fgetc (f)) != EOF) {
+        if (c=='\n') {
+            i++;
+            j=0;
+        }
+        else {
+            matrix[i][j++] = c;
         }
     }
     fclose (f);
 }
 
+void print () {
+    for (i=0; i<20; i++) {
+        for (j=0; j<20; j++) {
+            printf ("%c", matrix[i][j]);
+        }
+        printf ("\n");
+    }
+    printf ("\n");
+}
+
 int main () {
     maze_1 ();
+    print ();
+    char dice;
+    scanf ("%c", &dice);
+    matrix [0][5] = dice;
+    print ();
     return 0;
 }
