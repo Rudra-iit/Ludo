4264898c246d96776a11bb9016f2a4954e398b62
Rudra-iit <bsse1518@iit.du.ac.bd>
2024-12-19 11:50:52 +0600

    Snake ludo board.

---
+++ snake.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main () {
+    int board[10][10], i, j, k;
+    for (i=10-1; i>=0; i--) {
+        for (j=10-1; j>=0; j--) {
+            board[i][j] = i*10 + (j+1);
+        }
+    }
+    for (i=10-1; i>=0; i--) {
+        for (j=10-1; j>=0; j--) {
+            if (board[i][j]==100) {
+                printf ("%d ", board[i][j]);
+            }
+            else if (board[i][j]>=10 && board[i][j]<100) {
+                printf (" %d ", board[i][j]);
+            }
+            else {
+                printf ("  %d ", board[i][j]);
+            }
+        }
+        printf ("\n");
+    }
+    return 0;
+}
