cb8f56aa4a76ce396b81432869e5510d2dfc79e5
Rudra-iit <bsse1518@iit.du.ac.bd>
2025-01-13 10:19:17 +0600

    Random number in snake.
    I really wish it looked betteeeer...

--- snake.c
+++ snake.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main () {
-    int board[10][10], i, j, k;
-    for (i=10-1; i>=0; i--) {
-        for (j=10-1; j>=0; j--) {
-            board[i][j] = i*10 + (j+1);
-        }
-    }
+void print (int board[10][10]) {
+    int i, j;
     for (i=10-1; i>=0; i--) {
         for (j=10-1; j>=0; j--) {
             if (board[i][j]==100) {
@@ -21,5 +17,19 @@ int main () {
         }
         printf ("\n");
     }
+}
+
+int main () {
+    int board[10][10], i, j, m, n;
+    for (i=10-1; i>=0; i--) {
+        for (j=10-1; j>=0; j--) {
+            board[i][j] = i*10 + (j+1);
+        }
+    }
+    print (board);
+    int x = rand () % 12 + 1;
+    printf ("%d\n", x);
+    board[0][x] = 0;
+    print (board);
     return 0;
 }
