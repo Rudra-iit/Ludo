9c719249ecb3482812803bb4eade16cef767776c
Rudra-iit <bsse1518@iit.du.ac.bd>
2025-01-19 23:40:21 +0600

    first continuous loop to run snake.

--- snake.c
+++ snake.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int board[10][10], i, j;
+
 void print (int board[10][10]) {
-    int i, j;
+    printf ("\n");
     for (i=10-1; i>=0; i--) {
         for (j=10-1; j>=0; j--) {
             if (board[i][j]==100) {
@@ -20,16 +22,32 @@ void print (int board[10][10]) {
 }
 
 int main () {
-    int board[10][10], i, j, m, n;
     for (i=10-1; i>=0; i--) {
         for (j=10-1; j>=0; j--) {
             board[i][j] = i*10 + (j+1);
         }
     }
     print (board);
-    int x = rand () % 6 + 1;
-    printf ("%d\n", x);
-    board[0][x] = 0;
+    int y=0;
+    while (y<=50) {
+    int x = rand () % 5 + 1;
+    y = x+y;
+    int s = rand () % 90 + 1;
+    int e = rand () % 80 + 1;
+    printf ("\nThe dice roles %d, snake's head is %d and tail is %d\n", x, s, e);
+    for (i=0; i<10; i++) {
+        for (j=0; j<10; j++) {
+            if (y==s) {
+                if (board[i][j]==e) {
+                    board[i][j] = 0;
+                }
+            }
+            else if (board[i][j]==y) {
+                board[i][j] = 0;
+            }
+        }
+    }
     print (board);
+    }
     return 0;
 }
