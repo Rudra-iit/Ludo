bce8f0c95aed0ecf179fc3fa59d9f8f8e074da9b
Rudra-iit <bsse1518@iit.du.ac.bd>
2025-01-21 11:45:27 +0600

    new

--- ludo.cpp
+++ ludo.cpp
@@ -1,24 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char matrix[20][20];
-int i=0, j=0;
-
-typedef struct Node {
-    int x, y;
-    struct Node *left, *right, *up, *down;
-} Node;
-
-Node* make_node (int x, int y) {
-    Node* newNode = (Node*) malloc (sizeof (Node));
-    newNode->x = x;
-    newNode->y = y;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    newNode->up = NULL;
-    newNode->down = NULL;
-    return newNode;
-}
+char maze[20][20];
+int i, j;
 
 void maze_1 () {
     FILE *f = fopen ("maze_1.txt", "r");
@@ -33,58 +17,24 @@ void maze_1 () {
             j=0;
         }
         else {
-            matrix[i][j++] = c;
+            maze[i][j++] = c;
         }
     }
     fclose (f);
 }
 
-Node* tree (char matrix[20][20], int x, int y, int row, int column) {
-    if (x<0 || x>=row || y<0 || y>=column || matrix[x][y]=='#') {
-        return NULL;
-    }
-    Node* node = make_node (x, y);
-    matrix[x][y] = '#';
-    node->left = tree (matrix, x, y-1, row, column);
-    node->right = tree (matrix, x, y+1, row, column);
-    node->up = tree (matrix, x-1, y, row, column);
-    node->down = tree (matrix, x+1, y, row, column);
-    return node;
-}
-
 void print_matrix () {
     for (i=0; i<20; i++) {
         for (j=0; j<20; j++) {
-            printf ("%c", matrix[i][j]);
+            printf ("%c", maze[i][j]);
         }
         printf ("\n");
     }
     printf ("\n");
 }
 
-void print_tree (Node* root) {
-    if (root==NULL) {
-        return;
-    }
-    printf ("%d %d\n", root->x, root->y);
-    print_tree (root->left);
-    print_tree (root->right);
-    print_tree (root->up);
-    print_tree (root->down);
-}
-
-int main () {
+int main() {
     maze_1 ();
     print_matrix ();
-    Node* root = tree (matrix, 0, 5, 20, 20);
-    print_tree (root);
-    /*
-    matrix [0][5] = '$';
-    print_matrix ();
-    int x = rand () % 6 + 1;
-    printf ("%d\n", x);
-    matrix [x-3][5+3] = '$';
-    print_matrix ();
-    */
     return 0;
 }
